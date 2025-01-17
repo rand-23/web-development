881c6bd8c6dedb93fdd5f8652a723172b1a9b2ad
rand-23 <randshamsaldin01@gmail.com>
2025-01-17 13:26:17 +0300

    Create page.jsx

---
+++ ecommerce/app/checkout/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// Checkout functional component
+function Checkout() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className='text-center text-red-600 px-10 py-10 font-extrabold text-3xl'>THANK YOU FOR VISITING OUR APPLICATION</div>
+    </div>
+  
+  )
+}
+
+export default Checkout;  // Exporting the component for use in other parts of the application
