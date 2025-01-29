03a0975257ec776e7e5376fafadd7dec0480aa77
rand-23 <randshamsaldin01@gmail.com>
2025-01-29 22:22:58 +0300

    Update page.tsx

--- ecommerce/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ ecommerce/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,5 +2,9 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <SignUp />
+    </div>
+  )
 }
